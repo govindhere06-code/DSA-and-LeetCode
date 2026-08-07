1baa514111d4754395d9537d1abba77cc6536cd3
govindhere06-code <govindhere.06@gmail.com>
2026-08-07 19:10:49 +0530

    Arrays updated

---
+++ SecondLargestArray/solution.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def secondLargestElement(self, nums: List[int]) -> int:
+        largest = float('-inf')
+        second_largest = float('-inf')
+
+        for num in nums:
+
+            if num > largest:
+                second_largest = largest
+                largest = num
+
+            elif largest > num > second_largest:
+                second_largest = num
+
+        return second_largest
